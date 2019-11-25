70c30ad0a2aba7a6f31a1744017458fa80ae3ebd
zeeyon chung <zeeyon.c@gmail.com>
2019-11-25 22:08:14 +0900

    상품 도메인 개발

--- jpashop-2/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop-2/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("수량 부족");
+        }
+        this.stockQuantity = restStock;
+    }
 }
