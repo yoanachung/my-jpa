e028a336df1da61b80fe89ad6101e3949b32489c
Zeeyon <zeeyon.c@gmail.com>
2020-11-01 09:35:59 +0900

    페이징

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -31,8 +35,19 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * http://localhost:8080/members?page=3&size=5&sort=id,desc&sort=username
+     */
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("member1"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("member" + i, i));
+        }
     }
 }
